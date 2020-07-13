cb5894d7c98b826fa34992c60a6f6e7b9ecfa161
nivbrook <nivbrook@gmail.com>
2020-07-13 01:45:17 -0700

    Update settings.py

--- sneaker_shop/settings.py
+++ sneaker_shop/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,5 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
